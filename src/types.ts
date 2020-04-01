6c6f259db0e0e65a380e6d82ec0e8c44f403e73c
Eiker Yejan <eikeryejan@gmail.com>
2020-04-01 17:00:18 -0400

    Add types

---
+++ src/types.ts
@@ -0,0 +1,13 @@
+export type UserObject = {
+  gender: string
+  email: string
+  name: {
+    title: string
+    first: string
+    last: string
+  }
+  picture: {
+    large: string
+  }
+  cell: string
+}
