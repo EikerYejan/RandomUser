ea5739f9a02ce548213480cf92e194ae091e429c
Eiker Yejan <eikeryejan@gmail.com>
2020-04-02 12:27:44 -0400

    Add Firebase config

---
+++ src/config/firebase.ts
@@ -0,0 +1,10 @@
+export default {
+  apiKey: "AIzaSyB6_NwFF6MniFY0r9IxTqaJHq6mc936XwA",
+  authDomain: "random-users-e2b9f.firebaseapp.com",
+  databaseURL: "https://random-users-e2b9f.firebaseio.com",
+  projectId: "random-users-e2b9f",
+  storageBucket: "random-users-e2b9f.appspot.com",
+  messagingSenderId: "776039870587",
+  appId: "1:776039870587:web:f79bd30afe5dc7732632de",
+  measurementId: "G-Q7YZ5DSY7K",
+}
