ac2b54aafadccf273a8da36801f35bcb824f1109
Eiker Yejan <eikeryejan@gmail.com>
2020-04-02 13:08:58 -0400

    Add .env variables

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,10 +1,21 @@
+const {
+  REACT_APP_FIREBASE_API_KEY,
+  REACT_APP_FIREBASE_AUTH_DOMAIN,
+  REACT_APP_FIREBASE_DB_URL,
+  REACT_APP_FIREBASE_PROJECT_ID,
+  REACT_APP_FIREBASE_STORAGE,
+  REACT_APP_FIREBASE_MSG_SENDER_ID,
+  REACT_APP_FIREBASE_APP_ID,
+  REACT_APP_FIREBASE_MEASUREMENT_ID,
+} = process.env
+
 export default {
-  apiKey: "AIzaSyB6_NwFF6MniFY0r9IxTqaJHq6mc936XwA",
-  authDomain: "random-users-e2b9f.firebaseapp.com",
-  databaseURL: "https://random-users-e2b9f.firebaseio.com",
-  projectId: "random-users-e2b9f",
-  storageBucket: "random-users-e2b9f.appspot.com",
-  messagingSenderId: "776039870587",
-  appId: "1:776039870587:web:f79bd30afe5dc7732632de",
-  measurementId: "G-Q7YZ5DSY7K",
+  apiKey: REACT_APP_FIREBASE_API_KEY,
+  authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: REACT_APP_FIREBASE_DB_URL,
+  projectId: REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: REACT_APP_FIREBASE_STORAGE,
+  messagingSenderId: REACT_APP_FIREBASE_MSG_SENDER_ID,
+  appId: REACT_APP_FIREBASE_APP_ID,
+  measurementId: REACT_APP_FIREBASE_MEASUREMENT_ID,
 }
